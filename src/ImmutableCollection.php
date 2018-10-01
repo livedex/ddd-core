69560eb7a1ac6cd4e7fac32614fb5a2a2b588481
Lucio Merotta <l.merotta@gmail.com>
2018-10-01 22:44:36 +0200

    Extended ArrayIterator for ImmutableCollection

--- src/ImmutableCollection.php
+++ src/ImmutableCollection.php
@@ -12,39 +12,12 @@ namespace Pokedex\Core;
  * Class ImmutableCollection
  * @package Pokedex
  */
-class ImmutableCollection implements \ArrayAccess, \Traversable
+class ImmutableCollection extends \ArrayIterator
 {
     /**
      * @var array
      */
     protected $items = [];
-
-    /**
-     * ImmutableCollection constructor.
-     * @param array $items
-     */
-    public function __construct(array $items)
-    {
-        $this->items = $items;
-    }
-
-    /**
-     * @param mixed $offset
-     * @return bool
-     */
-    public function offsetExists($offset)
-    {
-        return !empty($this->items[$offset]);
-    }
-
-    /**
-     * @param mixed $offset
-     * @return mixed
-     */
-    public function offsetGet($offset)
-    {
-        return $this->items[$offset];
-    }
 
     /**
      * @param mixed $offset
@@ -62,5 +35,4 @@ class ImmutableCollection implements \ArrayAccess, \Traversable
     {
         throw new \InvalidArgumentException("Cannot unset value on an ImmutableCollection");
     }
-
 }
